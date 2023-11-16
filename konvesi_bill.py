edd658541e5cf6297945c5aad6f1b347e63bf6f5
nyomsas99 <nyomsas1234@gmail.com>
2023-11-16 10:01:16 +0700

    nfnf

--- konvesi_bill.py
+++ konvesi_bill.py
@@ -1,5 +1,5 @@
 while True:
-    print("1.Hexa Decimal to Decimal\n2.Decimal to Hexadecimal\n3.Exit")
+    print("1.Hexa Decimal to Decimal\n2.Decimal to Hexadecimal\n3.Calculate Resistor\n4.Exit")
     x = int(input("What want you to do?: "))
     if(x == 1):
         def hex_to_decimal(hex_number):
@@ -22,5 +22,31 @@ while True:
         decimal_number = int(input("Masukkan bilangan desimal: "))
         hex_result = decimal_to_hex(decimal_number)
         print(f"Hasil konversi: {hex_result}")
+    elif(x == 3):
+        def hitung_nilai_resistor(warna1, warna2, warna3, toleransi):
+            nilai_warna = {
+                'hitam': 0, 'coklat': 1, 'merah': 2, 'orange': 3, 'kuning': 4,
+                'hijau': 5, 'biru': 6, 'ungu': 7, 'abu-abu': 8, 'putih': 9
+            }
+
+            nilai = (nilai_warna[warna1] * 10 + nilai_warna[warna2]) * 10 ** nilai_warna[warna3]
+            toleransi_percent = {'emas': 5, 'perak': 10, 'tanpa warna': 20}
+
+            toleransi_value = nilai * toleransi_percent[toleransi] / 100
+
+            return nilai, toleransi_value
+
+        warna1 = input("Masukkan warna pertama: ").lower()
+        warna2 = input("Masukkan warna kedua: ").lower()
+        warna3 = input("Masukkan warna ketiga: ").lower()
+        toleransi = input("Masukkan toleransi (emas/perak/tanpa warna): ").lower()
+
+        nilai, toleransi_value = hitung_nilai_resistor(warna1, warna2, warna3, toleransi)
+
+        if(toleransi_value >= 1000 ):
+            kiloOhm = toleransi_value / 1000
+            print(f"Nilai Total Resistor: {kiloOhm} Kilo Ohm")
+        else:
+            print(f"Nilai Total Resistor: {toleransi_value} ohm")
     else:
         exit()
--- res_cal.py
+++ res_cal.py
@@ -1,13 +1,25 @@
-warna = [0,1,2,3,4,5,6,7,8,9]
-resistan = [0.05,0.10,0.20]
+def hitung_nilai_resistor(warna1, warna2, warna3, toleransi):
+    nilai_warna = {
+        'hitam': 0, 'coklat': 1, 'merah': 2, 'orange': 3, 'kuning': 4,
+        'hijau': 5, 'biru': 6, 'ungu': 7, 'abu-abu': 8, 'putih': 9
+    }
 
-c1 = str(input("Masukan warna pertama: "))
-c2 = str(input("Masukan warna kedua: "))
-c3 = str(input("Masukan warna ketiga: "))
-c4 = str(input("Masukan warna ke empat: "))
+    nilai = (nilai_warna[warna1] * 10 + nilai_warna[warna2]) * 10 ** nilai_warna[warna3]
+    toleransi_percent = {'emas': 5, 'perak': 10, 'tanpa warna': 20}
 
+    toleransi_value = nilai * toleransi_percent[toleransi] / 100
 
-if(c1 == "coklat" and c2 == "hitam"):
-   result = warna[1] * warna[0] * warna[3] * resistan[0]
-   print(result)
-   
+    return nilai, toleransi_value
+
+warna1 = input("Masukkan warna pertama: ").lower()
+warna2 = input("Masukkan warna kedua: ").lower()
+warna3 = input("Masukkan warna ketiga: ").lower()
+toleransi = input("Masukkan toleransi (emas/perak/tanpa warna): ").lower()
+
+nilai, toleransi_value = hitung_nilai_resistor(warna1, warna2, warna3, toleransi)
+
+if(toleransi_value >= 1000 ):
+   kiloOhm = toleransi_value / 1000
+   print(f"Nilai Total Resistor: {kiloOhm} Kilo Ohm")
+else:
+   print(f"Nilai Total Resistor: {toleransi_value} ohm")
